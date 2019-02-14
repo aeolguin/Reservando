589f365d9af6c5d3747ad1e6a9d030db75021f93
Ariel Olguin <arielolg@arielPC.arielpc.net>
2019-02-14 18:35:33 -0300

    iniciando

---
+++ js/test.js
@@ -0,0 +1,13 @@
+describe("probar la funcion X" , function(){
+
+    it("caso de test", function(){
+        
+        expect( resultado).to.equal(6);
+    });
+
+    it("caso de test", function(){
+
+    });
+
+
+});
