5a7092977e03342c5a77d64d4116e323dd4e8561
Ariel Olguin <arielolg@arielPC.arielpc.net>
2019-02-21 21:24:37 -0300

    falta solo un test

--- js/test.js
+++ js/test.js
@@ -3,25 +3,27 @@ const expect = chai.expect;
 describe("Testeando la clase Restaurant" , function(){
 
     const restaurante2 = listadoDeRestaurantes[2];
-    it("reservar Horario existente", function(){
+    let resultado = [];
+
+    it("reservar Horario existente - La funcion debe eliminar un horario del array horarios", function(){
         //La funcion debe eliminar un horario del array horarios
         restaurante2.reservarHorario("11:30");
         expect(restaurante2.horarios.length).to.equal(2);
     });
 
-    it("Reservar Horario inexitente", function(){
+    it("Reservar Horario inexitente - La funcion no debe eliminar nada del array", function(){
         //La funcion no debe eliminar nada del array
         restaurante2.reservarHorario("11:30");
         expect(restaurante2.horarios.length).to.equal(2);
     });
 
-    it("Reservar Horario String Vacío", function(){
+    it("Reservar Horario String Vacío - La funcion no debe eliminar nada del array", function(){
         //La funcion no debe eliminar nada del array
         restaurante2.reservarHorario("");
         expect(restaurante2.horarios.length).to.equal(2);
     });
 
-    it("Calculo de promedio", function(){
+    it("Calculo de promedio - se verifica que si el array no es vacío se calcula el promedio", function(){
         //se verifica que si el array no es vacío se calcula el promedio
         restaurante2.calificaciones = [2,2,2];
         if (restaurante2.calificaciones.length != 0){
@@ -29,13 +31,13 @@ describe("Testeando la clase Restaurant" , function(){
         }
     });
 
-    it("Calificacion en Cero", function(){
+    it("Calificacion en Cero - se verifica que si el array es vacío la calificación es Cero", function(){
         //se verifica que si el array es vacío la calificación es Cero
         restaurante2.calificaciones = [];
         expect(restaurante2.obtenerPuntuacion()).to.equal(0);
     });
 
-    it("Agregar calificación valida al Restaurante", function(){
+    it("Agregar calificación valida al Restaurante - Se recorren todos los valores posibles y se chequea que se agrega la calificación", function(){
         //Se recorren todos los valores posibles y se chequea que se agrega la calificación
         restaurante2.calificaciones = [];
         for (i=0;i<20;i++) {
@@ -46,13 +48,29 @@ describe("Testeando la clase Restaurant" , function(){
         expect(restaurante2.calificaciones.length).to.eql(9);
     });
 
-    it("Chequeo que Calificación no sea string vacío", function(){
+    it("Chequeo que Calificación no sea string vacío - Se chequea que no sea un string vacío ", function(){
         //Se chequea que no sea un string vacío 
         expect(restaurante2.calificar("")).to.eql(undefined);
     });
 
-    it("Chequeo que Calificación no sea string", function(){
+    it("Chequeo que Calificación no sea string - Se chequea que no sea un string", function(){
         //Se chequea que no sea un string   
         expect(restaurante2.calificar("a")).to.eql(undefined);
     });
+});
+
+describe("Testeando la clase Listado" , function(){
+    it("Existencia de un restaurante - La funcion chequea que exitan los 24 restaurantes", function(){
+        for (let i=1; i<25; i++) { 
+            resultado.push(listado.buscarRestaurante(i));
+        }
+        expect(resultado).to.have.lengthOf(24);
+    })
+
+    it("Existencia de un restaurante - La funcion chequea q se devuelva el valor que corresponde al restaurante solicitado", function(){
+        for (let i=0; i<24; i++) { 
+            expect(listado.restaurantes[i].nombre).to.eql(resultado[i].nombre);
+        }
+    })  
+
 });
